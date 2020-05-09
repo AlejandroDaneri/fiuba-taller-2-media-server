9cb59b693de2ed77c4d867b9106131b77abf1c4c
Alejandro Daneri <alejandrodaneri07@gmail.com>
2020-05-08 22:35:04 -0300

    fix CORS

--- app.js
+++ app.js
@@ -5,15 +5,6 @@ var routes = require('./src/routes')
 
 app.use('/', routes)
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', process.env.DOMAIN_ALLOWED)
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
-  next()
-})
-
 const server = app.listen(process.env.PORT, function () {
   console.info('Example app listening on port', process.env.PORT)
 })
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,15 @@ var router = express.Router()
 var Firebase = require('./firebase')
 var firebase = new Firebase()
 
+router.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', process.env.DOMAIN_ALLOWED)
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+})
+
 router.get('/list', function (req, res) {
   firebase
     .listVideoFiles()
