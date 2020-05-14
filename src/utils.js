cfbf91d31dffc6e62adb1dd48c11f9faa3bd9a75
Alejandro Daneri <alejandrodaneri07@gmail.com>
2020-05-14 17:46:14 -0300

    adding init script

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,21 @@
 module.exports = {
+  //   async update () {
+  //     const { Client } = require('pg')
+  //     const client = new Client({
+  //       connectionString: process.env.DATABASE_URL
+  //     })
+  //     await client.connect()
+  //     client.query('SELECT * FROM table')
+  //         .then(response => {
+  //             console.log(response.rows)
+  //             client.end()
+  //         })
+  //         .catch(err => {
+  //             client.end()
+  //         })
+  //     await client.end()
+  //   }
+
   async checkPostgres () {
     const { Client } = require('pg')
     const client = new Client({
