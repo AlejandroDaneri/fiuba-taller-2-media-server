22784ff291fc271a356e3926a6926fa0851050dc
Alejandro Daneri <alejandrodaneri07@gmail.com>
2020-05-23 12:07:04 -0300

    delete /list from routes

--- src/routes.js
+++ src/routes.js
@@ -2,25 +2,10 @@ var express = require('express')
 var router = express.Router()
 var videos = require('./videos')
 
-var Firebase = require('./firebase')
-var fb = new Firebase()
 var utils = require('./utils')
-var httpStatus = require('http-status-codes')
 
 router.use('/videos', videos)
 router.use(express.json())
-
-router.get('/list', function (req, res, next) {
-  fb.listVideoFiles()
-    .then(result => {
-      res.json(result)
-      console.info('Metadata request completed')
-    })
-    .catch(e => {
-      console.error(`Could not get files metadata: ${e}`)
-      res.status(httpStatus.INTERNAL_SERVER_ERROR).send('Error obtaining list')
-    })
-})
 
 router.get('/', function (req, res, next) {
   res.send('Hello World!')
