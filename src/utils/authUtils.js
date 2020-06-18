0e8e304a10fc58dd239a6e55c80e3c2083c5489e
Alejandro Daneri <alejandrodaneri07@gmail.com>
2020-06-17 21:39:37 -0300

    HOTFIX: api_key logger level passed to WARN

--- src/utils/authUtils.js
+++ src/utils/authUtils.js
@@ -1,17 +1,23 @@
 const errors = require('../errors/errors')
 const httpStatus = require('http-status-codes')
+const logger = require('../config/logger')
 
 module.exports = {
   clientApiKeyValidation (req, res, next) {
     const clientApiKey = req.get('X-Auth-Token')
     if (!clientApiKey) {
-      req.error = errors.response(-1, 'Missing Api Key')
-      req.error_code = httpStatus.UNAUTHORIZED
-      next(new Error('Missing Api Key'))
+      logger.warn('Missing Api Key')
+      return res
+        .status(httpStatus.UNAUTHORIZED)
+        .json(errors.response(-1, 'Missing Api Key'))
+      // req.error = errors.response(-1, 'Missing Api Key')
+      // req.error_code = httpStatus.UNAUTHORIZED
+      // next(new Error('Missing Api Key'))
     } else if (clientApiKey !== process.env.API_KEY) {
-      req.error = errors.response(-1, 'Invalid Api Key')
-      req.error_code = httpStatus.UNAUTHORIZED
-      next(new Error('Invalid Api Key'))
+      logger.warn('Invalid Api Key')
+      return res
+        .status(httpStatus.UNAUTHORIZED)
+        .json(errors.response(-1, 'Invalid Api Key'))
     }
     next()
   }
